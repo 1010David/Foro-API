c1825409bfaee9804a57668e31e14b0edd6e2c90
David <j.david.beltran.m@gmail.com>
2024-12-26 18:04:53 -0500

    Iniciando Security

--- src/main/java/com/aluraoracle/api_foro/usuario/UsuarioRepository.java
+++ src/main/java/com/aluraoracle/api_foro/usuario/UsuarioRepository.java
@@ -1,8 +1,11 @@
 package com.aluraoracle.api_foro.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     boolean existsByNombre(String nombre);
+
+    UserDetails findByUsername(String username);
 }
